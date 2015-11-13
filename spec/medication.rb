1a618d4c415876f09449604f4a180d7ba938ea3a
Marc Kohli (shadowdoc) <marckohli@gmail.com>
2015-11-13 12:23:06 -0800

    changes to medicaion for DSTU 2 closes #39

---
+++ spec/medication.rb
@@ -0,0 +1,45 @@
+# Medication Spec
+
+require 'rest-client'
+server = YAML.load_file('fhir_server.yml')
+
+resource = JSON.parse(File.read("Medication/levothyroxine.medication.json"))
+
+describe '#delete' do
+    result = RestClient.delete server[:url] + resource['resourceType'] + "/" + resource['id'],
+                               :params => {:_format => server[:format]}
+
+    it {expect(result.code).to eq 204}
+
+end
+
+describe '#put' do
+
+    begin
+        result = RestClient.put server[:url] + resource['resourceType'] + "/" + resource['id'],
+                                resource.to_json,
+                                :content_type => server[:format] + '+fhir',
+                                :params => {:_format => server[:format]}
+    rescue => e
+        puts e.inspect
+    end
+
+    it {expect(result.code).to eq 200}
+
+end
+
+describe '#get' do
+
+    result = RestClient.get server[:url] + resource['resourceType'] + "/" + resource['id'],
+                            :params => {:_format => server[:format]}
+
+    it {expect(result.code).to eq 200}
+
+    # need to remove the metadata and other keys from the server version
+    json = JSON.parse(result)
+    json.delete('meta')
+    json.delete('lastUpdated')
+
+    it {expect(json).to eq resource}
+
+end
