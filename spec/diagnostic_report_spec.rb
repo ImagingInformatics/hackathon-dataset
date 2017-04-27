8173e48ab998d4469dd18b636b75ff61107dbc72
Marc Kohli (shadowdoc) <marckohli@gmail.com>
2017-04-27 04:24:36 -0700

    works in progress updating specs to test each resource

--- spec/diagnostic_report_spec.rb
+++ spec/diagnostic_report_spec.rb
@@ -2,42 +2,56 @@
 
 server = YAML.load_file('fhir_server.yml')
 
-resource = JSON.parse(File.read("siim_andy_tcga-50-5072/DiagnosticReport/diagnostic_report_5072.ct499.json"))
+def test_diagnostic_report(resource, server)
 
-RSpec.describe '#delete' do
-    result = fhir_delete(server, resource)
-    it {expect(result.code).to be >= 200}
-    it {expect(result.code).to be <= 204}
+    RSpec.describe '#delete' do
+        result = fhir_delete(server, resource)
+        it {expect(result.code).to be >= 200}
+        it {expect(result.code).to be <= 204}
 
-end
-
-RSpec.describe '#put' do
-
-    begin
-        result = fhir_put(server, resource)
-    rescue => e
-        puts e.inspect
     end
 
-    it {expect(result.code).to eq 200}
+    RSpec.describe '#put' do
 
+        begin
+            result = fhir_put(server, resource)
+        rescue => e
+            puts e.inspect
+        end
+
+        it {expect(result.code).to eq 200}
+
+    end
+
+    RSpec.describe '#get' do
+
+        result = fhir_get(server, resource)
+
+        it {expect(result.code).to eq 200}
+
+        # need to remove the metadata and other keys from the server version
+        json = JSON.parse(result)
+        json.delete('meta')
+        json.delete('lastUpdated')
+
+
+        resource['performer']['reference'] = server[:url] + resource['performer']['reference']
+        resource['subject']['reference'] = server[:url] + resource['subject']['reference']
+
+        # Spark changes div formatting
+        # remove the spaces and line brakes to check if the content is stable
+        resource['text']['div'] = resource['text']['div'].gsub(/\n\n/, '').gsub(/ /, '')
+
+        json['text']['div'] = json['text']['div'].gsub(/\r\n/, '').gsub(/ /, '')
+
+        it {expect(json).to eq resource}
+
+    end
 end
 
-RSpec.describe '#get' do
-
-    result = fhir_get(server, resource)
-
-    it {expect(result.code).to eq 200}
-
-    # need to remove the metadata and other keys from the server version
-    json = JSON.parse(result)
-    json.delete('meta')
-    json.delete('lastUpdated')
-
-
-    resource['performer']['reference'] = server[:url] + resource['performer']['reference']
-    resource['subject']['reference'] = server[:url] + resource['subject']['reference']
-
-    it {expect(json).to eq resource}
-
+Dir.glob("**/DiagnosticReport/*") do |f|
+    resource = JSON.parse(File.read(f))
+    puts "Testing resource: #{f}"
+    test_diagnostic_report(resource, server)
+    break
 end
