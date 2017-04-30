50ae0cab04d7b593963cb7c261df2b3bdd33e07b
Marc Kohli (shadowdoc) <marckohli@gmail.com>
2017-04-30 07:31:46 -0700

    fixes for MedicationOrders

--- spec/medication_order_spec.rb
+++ spec/medication_order_spec.rb
@@ -4,40 +4,50 @@ server = YAML.load_file('fhir_server.yml')
 
 resource = JSON.parse(File.read("siim_sally-breastdx-01-0003/MedicationOrder/medication_order.breastdx.json"))
 
-RSpec.describe '#delete' do
-    result = fhir_delete(server, resource)
-    it {expect(result.code).to be >= 200}
-    it {expect(result.code).to be <= 204}
+def test_medication_order(server, resource)
+    RSpec.describe '#delete' do
 
-end
+        result = fhir_delete(server, resource)
+        it {expect(result.code).to be >= 200}
+        it {expect(result.code).to be <= 204}
 
-RSpec.describe '#put' do
-
-    begin
-        result = fhir_put(server, resource)
-    rescue => e
-        puts e.inspect
     end
 
-    it {expect(result.code).to eq 200}
+    RSpec.describe '#put' do
+
+        begin
+            result = fhir_put(server, resource)
+        rescue => e
+            puts e.inspect
+        end
+
+        it {expect(result.code).to eq 200}
+
+    end
+
+    RSpec.describe '#get' do
+
+        result = fhir_get(server, resource)
+
+        it {expect(result.code).to eq 200}
+
+        # need to remove the metadata and other keys from the server version
+        json = JSON.parse(result)
+        json.delete('meta')
+        json.delete('lastUpdated')
+
+        resource['patient']['reference'] = server[:url] + resource['patient']['reference']
+        resource['prescriber']['reference'] = server[:url] + resource['prescriber']['reference']
+        resource['medicationReference']['reference'] = server[:url] + resource['medicationReference']['reference']
+
+        fhir_resource_compare(server, resource, json)
+
+    end
 
 end
 
-RSpec.describe '#get' do
-
-    result = fhir_get(server, resource)
-
-    it {expect(result.code).to eq 200}
-
-    # need to remove the metadata and other keys from the server version
-    json = JSON.parse(result)
-    json.delete('meta')
-    json.delete('lastUpdated')
-
-    resource['patient']['reference'] = server[:url] + resource['patient']['reference']
-    resource['prescriber']['reference'] = server[:url] + resource['prescriber']['reference']
-    resource['medicationReference']['reference'] = server[:url] + resource['medicationReference']['reference']
-
-    it {expect(json).to eq resource}
-
+Dir.glob("**/MedicationOrder/*") do |f|
+    resource = JSON.parse(File.read(f))
+    puts "Testing resource: #{f}"
+    test_medication_order(server, resource)
 end
