1e0ce6f4070e41c42e7a117f48869379a2b3fecc
Mohannad Hussain <mk@go2mk.com>
2024-02-07 09:05:18 -0500

    Replced the Ruby upload script with a python one

---
+++ upload.py
@@ -0,0 +1,94 @@
+import os
+import json
+import yaml
+import logging
+import requests
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
+
+# The HAPI FHIR server requires dependent objects to be loaded first
+# TODO Upgrade the script so that dependencies can be resolved in a more automated, elegant fashion (reference detection) vs. this crude way
+load_order = ["ValueSet", "Device", "Organization", "StructureDefinition", "Medication", "Practitioner", "Patient",
+              "AllergyIntolerance", "AdverseReaction", "Alert", "MedicationRequest", "MedicationDispense",
+              "MedicationAdministration", "ImagingReference", "Observation", "Order", "OrderResponse", "Procedure",
+              "Endpoint", "ImagingStudy", "DiagnosticReport", "Specimen", "Condition", "DocumentReference"]
+
+if len(os.sys.argv) < 3:
+    raise ValueError("usage:\n\npython upload.py server_config.yml path_for_recursion")
+
+config_file = os.sys.argv[1]
+
+if not os.path.exists(config_file):
+    raise FileNotFoundError("Please specify a valid yml config file")
+else:
+    with open(config_file, 'r') as file:
+        server = yaml.safe_load(file)
+
+# Test a basic GET against the FHIR server
+try:
+    result = requests.get(server['url'] + "Patient", headers={'apikey': server['apikey']},
+                          params={'_format': server['format']})
+    result.raise_for_status()
+except requests.exceptions.RequestException as err:
+    log.error(err)
+    raise SystemExit(err)
+
+fhir = {}
+
+for root, dirs, files in os.walk(os.sys.argv[2]):
+    for file in files:
+        if file.endswith(".json"):
+            data_path = os.path.join(root, file)
+            log.info("Loading: {}".format(data_path))
+
+            with open(data_path, 'r') as data_file:
+                resource = json.load(data_file)
+
+            resource_type = resource.get("resourceType", None)
+            resource_id = resource.get("id", None)
+
+            if resource_id is None:
+                log.error("Error reading {}, make sure an ID is specified in the header comment".format(data_path))
+                #raise ValueError
+
+            fhir.setdefault(resource_type, []).append(resource)
+
+# Make sure we have load_order for all of our resource types
+missing_keys = set(fhir.keys()) - set(load_order)
+
+if missing_keys:
+    log.error("Resource Types missing from load order: {}".format(missing_keys))
+    raise ValueError
+
+for resource_type in load_order:
+    if resource_type in fhir:
+        for resource in fhir[resource_type]:
+            resource_id = resource["id"]
+
+            if resource_id is None:
+                log.error("Error reading {}, make sure an ID is specified in the header comment".format(resource))
+                raise ValueError
+
+            if resource_id == "random":
+                try:
+                    url = server['url'] + resource_type
+                    log.info("POST - {}".format(url))
+                    result = requests.post(url, json=resource, headers={'Content-Type': server['format'] + '+fhir', 'apikey': server['apikey']},
+                                           params={'_format': server['format']})
+                    result.raise_for_status()
+                except requests.exceptions.RequestException as e:
+                    raise SystemExit(e.response.text)
+            else:
+                try:
+                    url = server['url'] + resource_type + "/" + resource_id
+                    log.info("PUT - {}".format(url))
+                    result = requests.put(url, json=resource, headers={'Content-Type': server['format'] + '+fhir','apikey': server['apikey']},
+                                          params={'_format': server['format']})
+                    result.raise_for_status()
+                except requests.exceptions.RequestException as e:
+                    raise SystemExit(e.response.text)
+
+            result_json = result.json()
+            # log.info("Submission Status: {}".format(result_json["issue"][0]["diagnostics"]))
+
